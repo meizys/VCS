3152497fdd1308abb3ffc379bb166b247f038c6a
meizys <tomas.meizys@gmail.com>
2017-05-19 20:59:49 +0300

    php
    headeri ir footeris

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="css/styliux.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<title>NamųDaržas</title>
+</head>
+
+<header>
+	
+	<div class="overlay"></div>
+
+
+ <div class="navbar-fixed nav-wrapper">
+    <nav>
+     
+        <a href="#!" class="brand-logo center"><img class="jhead-top" src="images/head-top.png"></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">Home</a></li>
+          <li><a href="badges.html">About</a></li>
+          <li><a href="badges.html">Contacts</a></li>
+          <li><a href="badges.html">Source</a></li>
+        </ul>
+
+      <div class="jknav" class="col s12">
+        <a href="#!" class="breadcrumb">First</a>
+        <a href="#!" class="breadcrumb">Second</a>
+        <a href="#!" class="breadcrumb">Third</a>
+      </div>
+
+
+
+
+
+
+
+          </nav>
+
+  </div>
+
+
+<div class="title-block">
+	<h1 ><img class="jusai" src="images/usai.png"></h1>
+	<h5 >
+
+	<span></span>
+	Aukšti
+	<span></span>
+
+	</h5>
+
+	</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include (header.php); ?>
+  
+	<body>
+
+	<h2 class="MOK">Meet our kalnai</h2>
+
+  <div class="row">
+    <div class="col s12 m6 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kalnas1.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kalnas2.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+        <div class="col s12 m6 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kalnas3.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+<?php include (footer.php); ?>
