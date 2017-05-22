a2a46f8a4f720b78c5f10c6e004b73a69533bf82
meizys <tomas.meizys@gmail.com>
2017-05-22 21:07:07 +0300

    single story

--- story1.php
+++ story1.php
@@ -1,3 +1,15 @@
+ <?php include ('header.php'); ?>
+   <div class="mpt1">
+    <h1><img src="images/usai.png"></h1>
+<!--     <h5>
+    <span></span>
+    Istorijos apačioj
+    <span></span>
+    </h5>
+ -->  </div>
+
+</header>
+
  <div class="col s12 m7">
     <h2 class="header">Horizontal Card</h2>
     <div class="card horizontal">
@@ -6,11 +18,50 @@
       </div>
       <div class="card-stacked">
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
+          <p>I am a very simple card. I am good at containing small bits of information.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          <p>I am a very simple card. I am good at containing small bits of information.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
     </div>
   </div>
+
+
+    <div class="row">
+    <div class="col s12 m6 l6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kalnas1.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons"><link rel="stylesheet" type="text/css" href=""></i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. 
+            I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kalnas2.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. 
+            I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+ 
