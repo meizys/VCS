65618f3ac5edb45a78df59ceae0c865f158880a4
useltomas <useltomas@gmail.com>
2017-05-23 19:09:22 +0300

    usai

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="page-footer">
+<footer class="page-footer grey">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
@@ -8,16 +8,16 @@
               <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Tomas feisbukas</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Tomaš feisbukas</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/tomas.meizys">Tomas feisbukas</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/tomas.jusel">Tomaš feisbukas</a></li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="footer-copyright">
+          <div class="footer-copyright grey darken-1">
             <div class="container">
             © 2017 Copyright
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a class="grey-text text-lighten-4 right" href="https://www.youtube.com/watch?v=bQ2SVwiaixc">More links</a>
             </div>
           </div>
         </footer>
