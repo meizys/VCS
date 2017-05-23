2a19685c0fc51c99ec4b059a809464cf6ebb421a
useltomas <useltomas@gmail.com>
2017-05-23 20:38:15 +0300

    relative

--- portfolio.php
+++ portfolio.php
@@ -14,17 +14,17 @@
 </header>
 
  <div class="col s12 m7">
-    <h2 class="header">Horizontal Card</h2>
+    <h2 class="header kaciuStraipsnis portfolioPav">"Kas nutiktų, jei katei ar šuniui pašalintumėte ūsus?"</h2>
     <div class="card horizontal">
       <div class="card-image">
-        <img src="http://lorempixel.com/100/190/nature/6">
+        <img src="images/kaciukas.jpg" height="150" width="100">
       </div>
       <div class="card-stacked">
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
+          <p style="font-weight: bold;">Dažnas yra atkreipęs dėmesį į katės, žiurkės ar šuns ūsus. Dauguma net žino, kad katėms juos nukirpti nėra labai gerai (vienas kitas tokį eksperimentą greičiausiai slapta bandė atlikti vaikystėje). Dėl šunų ūsų jaudinamasi mažiau. Tačiau išties – kas tai per dalykas? Kam jie skirti ir kodėl tiek daug gyvūnų juos turi?</p>
         </div>
         <div class="card-action">
-          <a href="story1.php">This is a link</a>
+          <a href="story1.php">Spustelk!</a>
         </div>
       </div>
     </div>
