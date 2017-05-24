8b0e2f4238fb37c357bd078604613bf65d5a5687
meizys <tomas.meizys@gmail.com>
2017-05-24 19:21:41 +0300

    asdad

--- TomasM.php
+++ TomasM.php
@@ -13,8 +13,18 @@ include ('header.php'); ?>
   <div class="otherHeaders storiusai">
      <h1><img <?php displayRandomPhotoArea();?>></h1>
   </div>
-</header>
 
+  <div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/250/250/nature/1"></a>
+    <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
+  </div>
+
+
+
+</header>
 <body>
 
 
